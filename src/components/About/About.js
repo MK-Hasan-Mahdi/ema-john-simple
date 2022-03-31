8d2f6fe6f23a55651b967d35090cf7a54607dbda
mk-hasan <mkhasan6891@gmail.com>
2022-03-31 14:24:34 +0600

    first commit

---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>Ema-John About Loaded</h2>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Orders/Orders.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Orders = () => {
+    return (
+        <div>
+            <h2>Place Your Orders: </h2>
+        </div>
+    );
+};
+
+export default Orders;
